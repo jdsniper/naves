a7db7bb3523d15e415297c0d9a0f18aad7fc8ab8
jd <jd@DESKTOP-GVQ3J7E>
2020-01-30 10:26:52 +0100

    x

--- src/main/java/com/mycompany/navesarcade/App.java
+++ src/main/java/com/mycompany/navesarcade/App.java
@@ -1,14 +1,20 @@
 package com.mycompany.navesarcade;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  * JavaFX App
@@ -19,6 +25,7 @@ public class App extends Application {
     final short SCENE_WIDTH=640 ;
     short navePosX = (short)((SCENE_WIDTH)/2);
     short navePosY=50;        
+    byte naveDirection = 0;
     
     @Override
     public void start(Stage stage) {
@@ -31,57 +38,59 @@ public class App extends Application {
         
           short stickPosY = (short)((SCENE_HEIGHT)/2);
           byte naveSpeed = 4;
-          byte naveDirection = 0;
-//
-//         // DIBUJO DE LA PALA
-//        Rectangle rectStick = new Rectangle();
-//        rectStick.setWidth(10);
-//        rectStick.setHeight(stickHeight);
-//        rectStick.setX(SCENE_WIDTH - 40);
-//        rectStick.setY(stickPosY);
-//        rectStick.setFill(Color.WHITE);        
-//        root.getChildren().add(rectStick);
-
+          
+          
         //fondo
         Image fondo = new Image(getClass().getResourceAsStream("/imagenes/fondo.jpg"));
         ImageView imageViewFondo = new ImageView(fondo);
         root.getChildren().add(imageViewFondo);
 
         //nave x y L A
-        Rectangle naveBase = new Rectangle(navePosX,navePosY,25,7);
-        Rectangle naveCentral = new Rectangle(navePosX+5,navePosY-5,15,7);
-        Rectangle naveCabina = new Rectangle(navePosX+10,navePosY-10,5,7);
+        
+        //tomar posicion inicial del grupo
+        
+        
+        Rectangle naveBase = new Rectangle(0,0,25,7);
+        Rectangle naveCentral = new Rectangle(5,-5,15,7);
+        Rectangle naveCabina = new Rectangle(10,-10,5,7);
         //colorear nave
         naveBase.setFill(Color.WHITE);
         naveCentral.setFill(Color.WHITE);
         naveCabina.setFill(Color.WHITE);
-        //grupo nave
+                    //grupo nave
         Group nave = new Group();
-        root.getChildren().add(naveBase);
-        root.getChildren().add(naveCentral);
-        root.getChildren().add(naveCabina);
-        //MOVIMIENTO DE LA NAVE 
-        nave.setLayoutX(navePosX);
-        nave.setLayoutY(navePosY);
+        nave.getChildren().add(naveBase);
+        nave.getChildren().add(naveCentral);
+        nave.getChildren().add(naveCabina);
         //mostrar el grupo de la nave
-        root.getChildren().add(nave);
+                    root.getChildren().add(nave);
+        //Disparo de la nave
         
-        //dar movimientom colocarla en su sitio hacer q dispare
+        
+        
          // CONTROL DEL TECLADO
-//        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-//            public void handle(final KeyEvent keyEvent) {
-//                switch(keyEvent.getCode()) {
-//                    case UP:
-//                        naveDirection = -1;
-//                        break;
-//                    case DOWN:
-//                        naveDirection = 1;
-//                        break;
-//                }                
-//            }
-//        });
-        // ANIMACIÓN DE LA NAVE
-                    nave.setLayoutY(navePosX);
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            public void handle(final KeyEvent keyEvent) {
+                switch(keyEvent.getCode()) {
+                    case RIGHT:
+                        naveDirection = -1;
+                        break;
+                    case LEFT:
+                        naveDirection = 1;
+                        break;
+                    
+                }
+//                System.out.println(naveDirection);
+            }
+        });
+
+        Timeline timeline = new Timeline(
+            // 0.017 ~= 60 FPS
+            new KeyFrame(Duration.seconds(0.017), new EventHandler<ActionEvent>() {
+                public void handle(ActionEvent ae) {
+
+                    // ANIMACIÓN DE LA NAVE
+                    //nave.setLayoutY(navePosX);
                     navePosX += naveSpeed * naveDirection;
                     if(navePosX <= 0 || navePosX >= SCENE_WIDTH) {
                         naveDirection = 0;
@@ -93,9 +102,20 @@ public class App extends Application {
                         naveDirection = 0;
                         navePosX = (short)(SCENE_WIDTH);
                     }
-
-    
-     
+                    System.out.println(navePosX);
+        
+                    //MOVIMIENTO DE LA NAVE 
+                    nave.setTranslateX(navePosX);
+                    //nave.setLayoutY(navePosY);
+                }
+                
+            })
+            
+        );
+        
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play(); 
+        
     }   
 
     public static void main(String[] args) {
